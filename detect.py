da47260c20f5150e966973fcd8e83dd0879555c4
İkranur Güler <62421679+gulerikra@users.noreply.github.com>
2023-03-28 18:03:34 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -18,20 +18,9 @@ setup_logger()
 
 # Load configuration
 cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8  # set threshold for this model
-cfg.DATASETS.TRAIN = ("my_train",) # Train Verilerimiz Yapılandırma Dosyasına Kaydeder
-cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 2 # Çalışan Sayısı
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml") # Ağırlıkları Çeker ve Yapılandırma Dosyasına Ekler
-cfg.SOLVER.IMS_PER_BATCH = 2 # Batch Size
-cfg.SOLVER.BASE_LR = 0.001 # Learning Rate (Öğrenme Oranı)
-cfg.SOLVER.GAMMA = 0.05 # Learning Rate Azaltma Çarpımı
-cfg.SOLVER.STEPS = [500] # Learning Rate Azaltma Adım Sayısı
-cfg.TEST.EVAL_PERIOD = 200 # Eğitim Sırasında Modeli Değerlendirmek İçin Adım Sayısı
-cfg.DATASETS.TEST = ("my_test", ) # Tets Verilerimiz Yapılandırma Dosyasına Kaydeder
-
-cfg.SOLVER.MAX_ITER = 2000 # İterasyon Sayısı
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2 # Sınıf Sayısı
 cfg.MODEL.WEIGHTS = 'model_final.pth'
 predictor = DefaultPredictor(cfg)
